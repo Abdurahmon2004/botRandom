ee5a66b8d3b7f2c25ed42f97cad3a61226b43c27
Abdurahmon2004 <karimjonovabdurahmon70@gmail.com>
2024-07-24 13:52:30 +0500

    'sdsddssd'

--- routes/auth.php
+++ routes/auth.php
@@ -12,10 +12,10 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('refreshRegister', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('refreshRegister', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
